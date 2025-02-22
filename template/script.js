2ef31f58a247f79c03c2276b9446b875cef0cf5a
Antonio Chavez <antoniochavezdev@gmail.com>
2025-02-21 23:31:55 -0700

    86a6negn4 Save the Date & Countdown

--- template/script.js
+++ template/script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  startCountdown('2025-02-30T00:00:00');
+  startCountdown('2025-02-30T08:00:00');
 });
 
+function getElementById(id){
+  return document.getElementById(id);
+}
+
 function startCountdown(eventDate) {
-  const timerElement = document.getElementById('timer');
+  const daysEl = getElementById('days');
+  const hoursEl = getElementById('hours');
+  const minutesEl = getElementById('minutes');
+  const secondsEl = getElementById('seconds');
 
   function updateCountdown() {
     const now = new Date();
@@ -15,13 +22,16 @@ function startCountdown(eventDate) {
     let seconds = '00';
 
     if (diff >= 0) {
-        days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        days = Math.floor(diff / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
         hours = Math.floor((diff / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
         minutes = Math.floor((diff / (1000 * 60)) % 60).toString().padStart(2, '0');
         seconds = Math.floor((diff / 1000) % 60).toString().padStart(2, '0');
     } 
 
-    timerElement.textContent = `${days} día(s) ${hours}:${minutes}:${seconds}`;
+    daysEl.textContent = days;
+    hoursEl.textContent = hours;
+    minutesEl.textContent = minutes;
+    secondsEl.textContent = seconds;
   }
 
   updateCountdown();
